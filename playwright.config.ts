1546f1d3faa62273fa6ae332b231120964d38d20
AmyBlank <amy@atlasup.com>
2023-06-22 09:56:12 -0500

    Tests for add user

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from "dotenv";
 
 // Use process.env.PORT by default and fallback to port 3000
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 // Set webServer.url and use.baseURL with the location of the WebServer respecting the correct set port
-const baseURL = `http://localhost:${PORT}`
+const baseURL = `http://localhost:${PORT}`;
+
+dotenv.config({ path: '.env.test' })
+
 
 /**
  * See https://playwright.dev/docs/test-configuration.
