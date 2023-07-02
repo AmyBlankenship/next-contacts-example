ed2f814ca1d576a5e4d0ae6bfb61181c95cf6e78
AmyBlank <amy@atlasup.com>
2023-07-02 12:30:20 -0500

    Feature: add address  Add test is failing

--- e2e/001_addresses.spec.ts
+++ e2e/001_addresses.spec.ts
@@ -24,6 +24,10 @@ test.beforeAll(async () => {
   });
 });
 
+test.beforeEach(async({page}) => {
+  await page.goto('http://localhost:3000/');
+})
+
 test.afterEach(() => {
   testCleanUpAddresses();
 });
@@ -34,9 +38,6 @@ test.afterAll(() => {
 });
 
 test('default state', async ({ page, browserName}) => {
-  await page.goto('http://localhost:3000/');
-  //allows us to see newly added user
-  await page.reload();
   const user = testUsers[browserName];
   await page.getByRole('listitem')
     .filter({hasText: `${user!.first_name} ${user!.last_name}`})
@@ -44,3 +45,27 @@ test('default state', async ({ page, browserName}) => {
     .click();
   await expect(page.getByText('No addresses yet...')).toBeVisible();
 });
+
+test('adding address', async ({ page, browserName}) => {
+  const user = testUsers[browserName];
+  const address = addresses[browserName];
+  await page.getByRole('listitem')
+    .filter({hasText: `${user!.first_name} ${user!.last_name}`})
+    .locator('css=.select-friend')
+    .click();
+
+  await page.getByRole('link', { name: 'New Contact' }).click();
+  await page.getByLabel('Line 1').fill(address!.line_1);
+  await page.getByLabel('Line 2').fill(address!.line_2);
+  await page.getByLabel('City').fill(address!.city);
+  await page.getByLabel('State').fill(address!.state);
+  await page.getByLabel('Zipcode').fill(address!.zip);
+
+  await page.getByRole('button', {name: 'Create Contact'}).click();
+
+  each(address, async (line) => {
+    if (!!line) { // line 2 text sometimes empty
+      await expect(page.getByText(line)).toBeVisible();
+    }
+  });
+})
